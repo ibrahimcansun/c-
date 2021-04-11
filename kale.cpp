268fb7959f1eca13ed49bee72ad5ad9599d8fcf2
ibrahimcansun <60621945+ibrahimcansun@users.noreply.github.com>
2021-04-11 18:13:09 +0300

    Satranç tahtası üzerinde kalenin konumu
    Satranç tahtasına rastgele koyulan kalelerin birbirini yiyemeyecek şekilde konması.
    %50 rastgelelik
    %100 rastgelelik sonrasında eklenecek

---
+++ kale.cpp
@@ -0,0 +1,53 @@
+#include <iostream>//cout cin
+#include <ctime>//rand
+
+using namespace std;
+
+bool KonumuKontrolEt(char[8][8], int, int);
+void SonKonumuBas(char[8][8]);
+
+int main(){
+	char kaleKonumu[8][8];//satranc tahtam
+	int sayac = 0;
+	int rastgeleKonum;
+	
+	for(int a=0; a<8; a++){
+		for(int b=0; b<8; b++){
+			kaleKonumu[a][b] = '*';//bos eleman ataniyor
+		}
+	}
+	srand(time(0));//rastgelelik sagla
+	
+	while(sayac != 8){
+		rastgeleKonum = rand() % (8);
+		if(KonumuKontrolEt(kaleKonumu, sayac, rastgeleKonum)){
+			//yatayda veya dikeyde bir kale var
+		}else{
+			kaleKonumu[sayac][rastgeleKonum] = 'K';
+			sayac++;
+			cout << endl <<endl << sayac << endl;
+			SonKonumuBas(kaleKonumu);
+		}
+	}
+	return 0;
+}
+
+void SonKonumuBas(char kaleKonum[8][8]){
+	for(int a=0; a<8; a++){
+		for(int b=0; b<8; b++){
+			cout << kaleKonum[a][b] << " ";
+		}
+		cout << endl;
+	}
+}
+
+bool KonumuKontrolEt(char kaleKonum[8][8], int say, int rastKonum){
+	for(int i = 0; i < 8; i++){
+		if(kaleKonum[say][i] == 'K'){
+			return true;
+		}else if(kaleKonum[i][rastKonum] == 'K'){
+			return true;
+		}
+	}
+	return false;
+}
